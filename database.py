41759d685a54d6c6dd44bf0d6f95e562583d9d9b
punyiC <154292583+punyiC@users.noreply.github.com>
2025-05-15 13:13:25 +0900

    Add files via upload

---
+++ database.py
@@ -0,0 +1,10 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
+client = AsyncIOMotorClient(MONGO_URI)
+db = client["sos_database"]
+collection = db["sos_alerts"]
